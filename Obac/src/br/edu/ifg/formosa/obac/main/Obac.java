7ba79f39695ac8a3e4c52592e7037e51c1fd5e30
jeferson.jlma@gmail.com <jeferson.jlma@gmail.com@1db8cb58-839b-9c60-f9b3-5ae4c6cf1432>
2012-06-13 21:18:32 +0000

    Comit Inicial

---
+++ Obac/src/br/edu/ifg/formosa/obac/main/Obac.java
@@ -0,0 +1,24 @@
+
+package br.edu.ifg.formosa.obac.main;
+
+import java.applet.Applet;
+
+
+public class Obac extends Applet{
+
+   
+    public void init() {
+    	
+    	try {
+            java.awt.EventQueue.invokeAndWait(new Runnable() {
+
+                public void run() {
+                    //initComponents();
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+}
