879af3ed92bc1cebc738d943555ced600c3cbe3f
alvarolucasjs@gmail.com <alvarolucasjs@gmail.com@1db8cb58-839b-9c60-f9b3-5ae4c6cf1432>
2012-10-13 21:16:19 +0000

    Alterações

--- trunk/Obac/src/br/edu/ifg/formosa/obac/main/Obac.java
+++ trunk/Obac/src/br/edu/ifg/formosa/obac/main/Obac.java
@@ -14,7 +14,10 @@ public class Obac extends Applet{
 	private static final long serialVersionUID = 1L;
 
 	public void init() {
-    	
+		
+		this.setSize(800, 600);
+		this.setLayout(null);
+		
     	try {
              java.awt.EventQueue.invokeAndWait(new Runnable() {
 
