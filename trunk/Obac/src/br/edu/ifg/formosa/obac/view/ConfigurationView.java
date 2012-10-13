db55b3fc2043ea5035967c75c48043136db45b53
alvarolucasjs@gmail.com <alvarolucasjs@gmail.com@1db8cb58-839b-9c60-f9b3-5ae4c6cf1432>
2012-10-13 19:14:51 +0000

    alterações em CofigurationView

--- trunk/Obac/src/br/edu/ifg/formosa/obac/view/ConfigurationView.java
+++ trunk/Obac/src/br/edu/ifg/formosa/obac/view/ConfigurationView.java
@@ -1,5 +1,32 @@
 package br.edu.ifg.formosa.obac.view;
 
-public class ConfigurationView {
+import java.awt.Color;
+import java.awt.Label;
+import java.awt.Panel;
 
+
+import javax.swing.JPanel;
+
+public class ConfigurationView extends JPanel{
+	public ConfigurationView(){
+		
+		this.setLayout(null);
+		this.setSize(200, 600);
+		this.setBackground(Color.green);
+		
+		Label lbConfiguration = new Label("Configurações");
+		Label lbPropulsion = new Label("Propulsão");
+		
+		lbConfiguration.setBounds(0, 0, 100, 20);
+		lbPropulsion.setBounds(0, 30, 100, 20);
+				
+		Panel pPropulsion = new Panel(null);
+	
+		
+	// ---------------- add---------------
+		this.add(lbConfiguration);
+		this.add(lbPropulsion);
+	}
+	
+	
 }
