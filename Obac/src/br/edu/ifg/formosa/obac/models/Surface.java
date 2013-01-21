f1f8adde60a15f84ad1f5af16d2d929557b24cae
muriloosilva@gmail.com <muriloosilva@gmail.com@1db8cb58-839b-9c60-f9b3-5ae4c6cf1432>
2013-01-21 20:46:39 +0000

    alterações

--- Obac/src/br/edu/ifg/formosa/obac/models/Surface.java
+++ Obac/src/br/edu/ifg/formosa/obac/models/Surface.java
@@ -1,10 +1,12 @@
 package br.edu.ifg.formosa.obac.models;
 
+import br.edu.ifg.formosa.obac.utils.Conversion;
+
 public class Surface {
 	public static final double woodFriction = 0.62;
 	public static final double asphalt = 0.80;
 	private double coefFriction;
-	public static double width = 550.00;
+	public static double width = 500.00;
 	private double forcaAtrito;
 	private double escala;
 	private long posicaoFinal;
@@ -31,7 +33,7 @@ public class Surface {
 		this.forcaAtrito = forcaAtrito;
 	}
 	public double getEscala() {
-		return escala;
+		return Conversion.rounding(2, escala);
 	}
 	public void setEscala(double escala) {
 		this.escala = escala;
