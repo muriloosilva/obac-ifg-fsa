3958ab248acffd24bd9f81259ae922b5d4544697
ruanskt.fsa@gmail.com <ruanskt.fsa@gmail.com@1db8cb58-839b-9c60-f9b3-5ae4c6cf1432>
2013-01-24 20:47:39 +0000

    Alterações

--- Obac/src/br/edu/ifg/formosa/obac/view/InfoPanelView.java
+++ Obac/src/br/edu/ifg/formosa/obac/view/InfoPanelView.java
@@ -19,50 +19,86 @@ public class InfoPanelView extends JPanel{
 		this.setOpaque(false);
 		//this.setBackground(Color.white);
 		
-		
+		//Gravidade
 		TransparentLabel lbGravity = new TransparentLabel("Gravidade:");
-		lbGravity.setBounds(10, 3, 70, 20);
+		lbGravity.setBounds(10, 2, 70, 20);
 		this.add(lbGravity);
 		
 		TransparentLabel lbGravityValue = new TransparentLabel("1.1");
-		lbGravityValue.setBounds(80, 3, 70, 20);
+		lbGravityValue.setBounds(70, 2, 70, 20);
 		this.add(lbGravityValue);
 		
+		//Coef. de Atrito
 		TransparentLabel lbCoefAtrito = new TransparentLabel("Coef. Atrito:");
-		lbCoefAtrito.setBounds(10, 20, 70, 20);
+		lbCoefAtrito.setBounds(10, 18, 70, 20);
 		this.add(lbCoefAtrito);
 		
 		TransparentLabel lbCoefAtritoValue = new TransparentLabel("1.1");
-		lbCoefAtritoValue.setBounds(80, 20, 70, 20);
+		lbCoefAtritoValue.setBounds(70, 18, 70, 20);
 		this.add(lbCoefAtritoValue);
 		
+		//Força de Atrito
 		TransparentLabel lbForçaAtrito = new TransparentLabel("Força Atrito:");
-		lbForçaAtrito.setBounds(110, 3, 70, 20);
+		lbForçaAtrito.setBounds(110, 2, 70, 20);
 		this.add(lbForçaAtrito);
 		
 		TransparentLabel lbForçaAtritoValue = new TransparentLabel("1.1");
-		lbForçaAtritoValue.setBounds(195, 3, 70, 20);
+		lbForçaAtritoValue.setBounds(185, 2, 70, 20);
 		this.add(lbForçaAtritoValue);
 		
+		//Força Normal
 		TransparentLabel lbForçaNormal = new TransparentLabel("Forca Normal:");
-		lbForçaNormal.setBounds(110, 20, 80, 20);
+		lbForçaNormal.setBounds(110, 18, 80, 20);
 		this.add(lbForçaNormal);
 		
 		TransparentLabel lbForçaNormalValue = new TransparentLabel("1.1");
-		lbForçaNormalValue.setBounds(110, 20, 80, 20);
+		lbForçaNormalValue.setBounds(185, 18, 80, 20);
 		this.add(lbForçaNormalValue);
 		
+		//Pos. Final
 		TransparentLabel lbPosFinal = new TransparentLabel("Pos. Final:");
-		lbPosFinal.setBounds(222, 3, 70, 20);
+		lbPosFinal.setBounds(250, 2, 70, 20);
 		this.add(lbPosFinal);
 		
+		TransparentLabel lbPosFinalValue = new TransparentLabel("1.1");
+		lbPosFinalValue.setBounds(315, 2, 70, 20);
+		this.add(lbPosFinalValue);
+		
+		//Aceleração
 		TransparentLabel lbAceleração = new TransparentLabel("Aceleração:");
-		lbAceleração.setBounds(222, 20, 80, 20);
+		lbAceleração.setBounds(250, 18, 80, 20);
 		this.add(lbAceleração);
 		
+		TransparentLabel lbAceleraçãoValue = new TransparentLabel("1.1");
+		lbAceleraçãoValue.setBounds(315, 18, 80, 20);
+		this.add(lbAceleraçãoValue);
+		
+		//Tempo
 		TransparentLabel lbTempo = new TransparentLabel("Tempo:");
-		lbTempo.setBounds(330, 3, 45, 20);
+		lbTempo.setBounds(360, 2, 45, 20);
 		this.add(lbTempo);
+		
+		TransparentLabel lbTempoValue = new TransparentLabel("1.1");
+		lbTempoValue.setBounds(402, 2, 45, 20);
+		this.add(lbTempoValue);
+		
+		//Pos. Atual Eixo x:
+		TransparentLabel lbPosAtualEixox = new TransparentLabel("Pos. Atual Eixo x:");
+		lbPosAtualEixox.setBounds(445, 2, 100, 20);
+		this.add(lbPosAtualEixox);
+		
+		TransparentLabel lbPosAtualEixoxValue = new TransparentLabel("1.1");
+		lbPosAtualEixoxValue.setBounds(537, 2, 100, 20);
+		this.add(lbPosAtualEixoxValue);
+		
+		//Pos. Atual Eixo y:
+		TransparentLabel lbPosAtualEixoy = new TransparentLabel("Pos. Atual Eixo y:");
+		lbPosAtualEixoy.setBounds(445, 18, 100, 20);
+		this.add(lbPosAtualEixoy);
+				
+		TransparentLabel lbPosAtualEixoyValue = new TransparentLabel("1.1");
+		lbPosAtualEixoyValue.setBounds(537, 18, 100, 20);
+		this.add(lbPosAtualEixoyValue);
 		
 		
 	}
@@ -70,7 +106,7 @@ public class InfoPanelView extends JPanel{
 	public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        Color ppColor = new Color(0, 0, 0, 150); //r,g,b,alpha
+        Color ppColor = new Color(0, 0, 0, 115); //r,g,b,alpha
         g.setColor(ppColor);
         g.fillRect(0,0,600,40); //x,y,width,height
     }    
