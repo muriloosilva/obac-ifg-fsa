fa6cd1be904cbd1bd6fae6b00ecc033679267523
geraldo.senshi@gmail.com <geraldo.senshi@gmail.com@1db8cb58-839b-9c60-f9b3-5ae4c6cf1432>
2013-02-08 15:07:31 +0000

    alterações

--- Obac/src/br/edu/ifg/formosa/obac/main/Obac.java
+++ Obac/src/br/edu/ifg/formosa/obac/main/Obac.java
@@ -2,6 +2,8 @@
 package br.edu.ifg.formosa.obac.main;
 
 import java.applet.Applet;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
 
 
@@ -14,9 +16,15 @@ public class Obac extends Applet{
 	private static final long serialVersionUID = 1L;
 
 	public void init() {
-		
 		this.setSize(800, 600);
 		this.setLayout(null);
+		AccessController.doPrivileged(new PrivilegedAction() {
+		@Override
+		public Object run() {
+			
+			return null;
+		}
+		});
 		
     	try {
              java.awt.EventQueue.invokeAndWait(new Runnable() {
